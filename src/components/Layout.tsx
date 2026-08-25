9eeb6f345f9854a9c411c52a929f94ae113b3bae
Nate <96822493+King-odysseus@users.noreply.github.com>
2026-08-25 17:33:44 +0100

    feat: reorder theme toggle to light, dark, auto

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,7 +25,7 @@ const links = [
 
 type Theme = 'system' | 'light' | 'dark'
 
-const themeOrder: Theme[] = ['system', 'light', 'dark']
+const themeOrder: Theme[] = ['light', 'dark', 'system']
 
 function ThemeToggle() {
   const [theme, setTheme] = useState<Theme>('system')
